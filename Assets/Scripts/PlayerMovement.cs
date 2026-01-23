578423d162c2984197e0ad33b3682dbaf5b6f180
nttrang29 <thutrang.ntt215@gmail.com>
2026-01-23 16:26:46 +0700

    update BTVN demo part 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+    }
+}
