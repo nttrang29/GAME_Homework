ba0fb0b1e13bd2bca7826a0361df273d4a5c199c
nttrang29 <thutrang.ntt215@gmail.com>
2026-03-05 09:52:07 +0700

    Lab C6 animation

---
+++ Assets/Scripts/PlayerController_Lab8.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerController_Lab8_Fixed : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private int speedHash;
+    private int attackHash;
+
+    private float lastSpeed;
+
+    void Awake()
+    {
+        speedHash = Animator.StringToHash("Speed");
+        attackHash = Animator.StringToHash("Attack");
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleAttack();
+    }
+
+    private void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        float targetSpeed = Mathf.Abs(moveInput);
+
+        transform.Translate(Vector3.right * moveInput * moveSpeed * Time.deltaTime);
+
+        if (!Mathf.Approximately(lastSpeed, targetSpeed))
+        {
+            anim.SetFloat(speedHash, targetSpeed);
+            lastSpeed = targetSpeed;
+        }
+
+        if (moveInput > 0.1f) transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < -0.1f) transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void HandleAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(attackHash);
+        }
+    }
+}
