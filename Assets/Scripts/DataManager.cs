e3b560a08f1fb0bd3f9cf7c48e42daa16d58caec
nttrang29 <thutrang.ntt215@gmail.com>
2026-03-23 16:03:09 +0700

    Update

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -3,7 +3,7 @@
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
-    public string playerName = "Vudun";
+    public string playerName = "ThuTrang";
     public int score = 0;
 
     void Awake()
